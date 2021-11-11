bd52e5919aeba86802dcc0d0a9ce7f0fa66052bf
tomasmerlo <tmerlo@udesa.edu.ar>
2021-11-11 20:20:38 -0300

    comienzo de favoritos y boton

---
+++ js/favoritos.js
@@ -0,0 +1,84 @@
+
+
+
+// selecciona btn favoritos
+    
+const fav = document.querySelector(".fav");
+
+    // Paso 1: definir array para guardar lista de favoritos.
+    
+    
+let afavoritos = [];
+
+    // Paso 2: recuperamos datos del storage para ver si ya hay favoritos
+    
+    // puede pasar que la posición favoritos aún no exista,
+    // para ese caso definimos en el "paso 1" la variable favoritos.
+
+    // Paso 3: 
+    // si Ya se definió la propiedad "favoritos" en storage
+    // y si YA hay elementos dentro de localStorege
+    if ( x ) {
+
+        // Paso 4: ... transformamos ese string en array
+        // y le asignamos ese array a la variable "favoritos"
+        
+      }
+  
+      // Paso 5: Si el ID del gif actual está en la lista.
+      if ( x ) {
+  
+        // Se cambia el contenido del link favoritos.
+        
+      }
+  
+      /*
+      hasta aquí, instrucciones para cuando
+      se llega a la página
+  
+      a partir del paso 6, las instrucciones
+      para cuando se cliquea en el link
+      */
+  
+  
+      /*
+      Paso 6: Agregar/sacar gif actual a favoritos
+      */
+  
+      // evento: cuando se cliquee en el link fav
+      
+        
+        // evitamos el comportamiento default del link
+        
+        
+        // Si el gif actual está en la lista
+        if ( x ) {
+            
+          // lo localizamos en el array
+          
+          // y lo sacamos de alli
+          
+            
+          // luego --> cambiamos el contenido de link fav
+          
+            
+          // chequeamos en consola si funciona
+          
+            
+        } else { //Si no está en la lista,
+            
+          // Se agrega el gif actual
+          
+            
+          // luego --> se cambia el contenido del link fav
+          
+        }
+  
+  
+        // Paso 7.a: se guarda el array actualizado como string
+        
+        
+        // Paso 7.b: se guarda ese string en localStorage
+        
+        
+        // se verifica en consola como quedó todo
