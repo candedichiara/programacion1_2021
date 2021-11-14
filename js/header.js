a61bf1718b3e873ecccf9123b8a06679ce1f5360
candeladichiara <cdichiara@udesa.edu.ar>
2021-11-14 16:42:04 -0300

    Header terminado

--- js/header.js
+++ js/header.js
@@ -5,7 +5,12 @@ let buscar=document.querySelector(".buscar")
 
 formulario.addEventListener("submit",function(event){
     event.preventDefault()
-    if(buscar.value.length==0){}
+    if(buscar.value.length<3){
+        alert("La busqueda debe tener al menos 3 caracteres")
+    }
+    else{
+        this.submit()
+    }
 })
 
 
