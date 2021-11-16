98ab5b9ffb43e702dad7bd8eac7cad8156e6e81f
vicente544 <82721136+vicente544@users.noreply.github.com>
2021-11-16 08:42:35 -0300

    detail series

---
+++ js/detail-series.js
@@ -0,0 +1,37 @@
+window.addEventListener("load",function(){
+
+    let string = location.search
+    let url = new URLSearchParams(string);
+   let id = url.get("id")
+   fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=583130cf3b1ec0008be028545f8f0cff&language=en-US
+   `)
+.then(function(info){
+    return info.json()
+})
+ .then(function(response){
+    let article = document.querySelector(".articlepeakyblinders")
+    let generos = response.genres
+    console.log (response.genres)
+    article.innerHTML +=`
+    <h1 class="titulopeaky">${response.title}</h1>
+    <h3 class="textopeaky">${response.vote_average}/10</h3>`
+
+
+    generos.forEach(genero => {
+        article.innerHTML += `<h2 class="textopeaky">${genero.name}</h2>`
+    });
+    article.innerHTML+=
+    `<h3 class="textopeaky">${response.release_date}</h3>
+    <h4 class="textopeaky">${response.runtime}mins</h4>`
+
+    let imagenes = document.querySelector(".fotopb")
+imagenes.innerHTML +=
+    `<img class="fotopb" src="https://api.themoviedb.org/3/tv/${response.poster_path}/images?api_key=583130cf3b1ec0008be028545f8f0cff&language=en-US">`
+
+    console.log(response)
+let parrafo = document.querySelector(".sinopsispeaky")
+parrafo.innerText += response.overview
+})
+
+
+})
